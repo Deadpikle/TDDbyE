429e8db185db5d9d22faf328f685a40da034b449
Deadpikle <mikebobby@sbcglobal.net>
2016-07-13 18:22:06 -0400

    Initial Commit

---
+++ TDDByExample/TDDByExample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TDDByExample
+//
+//  Created by School of Computing Macbook on 7/13/16.
+//  Copyright © 2016 CIRC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
